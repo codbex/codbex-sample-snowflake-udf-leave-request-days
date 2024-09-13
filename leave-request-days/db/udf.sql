2f5db16d6f62b3799a00c71cffd920d2fe464973
iliyan-velichkov <velichkov.iliyan@gmail.com>
2024-09-13 14:07:48 +0000

    add update sql as example

--- leave-request-days/db/udf.sql
+++ leave-request-days/db/udf.sql
@@ -1,3 +1,4 @@
+-- create the UDF
 USE ROLE CONTAINER_USER_ROLE;
 USE DATABASE CONTAINER_HOL_DB;
  
@@ -7,4 +8,12 @@ CREATE OR REPLACE FUNCTION calculate_leave_request_days (countryIsoCode string, 
   ENDPOINT='app-endpoint'
   AS '/public/ts/leave-request-days/api/leave-request-days-service.ts';
 
+-- test the function with select
 SELECT FROM_DATE, TO_DATE, calculate_leave_request_days(COUNTRY_ISO_CODE, FROM_DATE, TO_DATE) as leave_days FROM LEAVE_REQUESTS;
+
+-- update entries using the function
+UPDATE LEAVE_REQUESTS
+SET REQUESTED_DAYS = calculate_leave_request_days(COUNTRY_ISO_CODE, FROM_DATE, TO_DATE);
+
+-- check the result
+SELECT * FROM LEAVE_REQUESTS;
